52686e568a60856efd8fda30f8db39cdad94941e
Adamonis Arseni <arsenaloo97@gmail.com>
2022-09-29 15:13:36 +0300

    15. Redux Extended Tools

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,25 @@
-import { compose, createStore, applyMiddleware } from 'redux';
-import logger from 'redux-logger';
+import {compose, createStore, applyMiddleware} from 'redux';
+import {persistStore, persistReducer} from "redux-persist";
+import storage from 'redux-persist/lib/storage';
+import {loggerMiddleware} from './middleware/logger'
 
-import { rootReducer} from "./root-reducer";
+import {rootReducer} from "./root-reducer";
 
-const middleWares = [logger];
+const middleWares = [process.env.NODE_ENV === 'development' && loggerMiddleware].filter(Boolean);
 
-const composeEnhancers = compose(applyMiddleware(...middleWares));
+const persistConfig = {
+    key: 'root', storage, blacklist: ['user']
+}
 
-export const store = createStore(rootReducer, undefined, composeEnhancers);
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const composeEnhancer = (process.env.NODE_ENV !== 'production' &&
+        window &&
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+    compose
+
+const composeEnhancers = composeEnhancer(applyMiddleware(...middleWares));
+
+export const store = createStore(persistedReducer, undefined, composeEnhancers);
+
+export const persistor = persistStore(store)
